bb129711a35392423e868ce313d660f3b080e0cc
Buket Zeren <84253467+b-zeren@users.noreply.github.com>
2022-03-11 18:09:52 +0300

    Update crop_video.py
    changed crop sizes

--- crop_video.py
+++ crop_video.py
@@ -7,12 +7,12 @@ cap = cv2.VideoCapture("./shep.mp4")
 result, frame = cap.read()
 h,w = frame.shape[:2]
 size = (w,h)
-out_size=(w,h//2)
+out_size=(w,3*(h//4))
 out = cv2.VideoWriter('./shep_cropped.mp4',cv2.VideoWriter_fourcc(*'mp4v'),30,out_size)
 while cap.isOpened():
     ret, frame = cap.read()
     if ret:
-        out_frame=frame[h//4:3*(h//4),:]
+        out_frame=frame[:3*(h//4),:]
         out.write(out_frame)
     else:
         print("An error occured")
