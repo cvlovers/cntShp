1f53220d77538f871e90f71f4b128b10782c309e
Buket Zeren <84253467+b-zeren@users.noreply.github.com>
2022-03-11 14:55:04 +0300

    Add files via upload
    To remove occlusion, we crop half of the frame. Run this script first and give shep_cropped to object_tracker script

---
+++ crop_video.py
@@ -0,0 +1,22 @@
+import cv2
+import numpy as np
+
+
+cap = cv2.VideoCapture("./shep.mp4")
+
+result, frame = cap.read()
+h,w = frame.shape[:2]
+size = (w,h)
+out_size=(w,h//2)
+out = cv2.VideoWriter('./shep_cropped.mp4',cv2.VideoWriter_fourcc(*'mp4v'),30,out_size)
+while cap.isOpened():
+    ret, frame = cap.read()
+    if ret:
+        out_frame=frame[h//4:3*(h//4),:]
+        out.write(out_frame)
+    else:
+        print("An error occured")
+        break
+
+out.release()
+cap.release()
