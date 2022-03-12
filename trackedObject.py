e9952dae2191178779caa7e1cde6474d65d0c67e
Buket Zeren <84253467+b-zeren@users.noreply.github.com>
2022-03-12 14:44:18 +0300

    No movement threshold is auto calculated now, if line is passed disable

--- trackedObject.py
+++ trackedObject.py
@@ -9,23 +9,28 @@ class trackableObject:
         self.tracker=None
         self.id=id
 
-    def updateLoc(self,new_location):
+    def updateLoc(self,new_location,w,h):
         self.diff=((new_location[0]-self.loc[0])**2+(new_location[1]-self.loc[1])**2)**(0.5)
         self.loc=list(map(int,new_location))
-        if self.diff<30:
+        diff_threshold=0.01*((w*h)**0.5)
+        #center = (x+w/2,y+h/2)
+        center=(self.loc[0]+self.loc[2]//2,self.loc[1]+self.loc[3]//2) 
+        
+        if self.diff<diff_threshold:
             self.missing_frame+=1
         else:
             self.missing_frame=0
-        if self.loc[0]<0 or self.loc[1]<0 or self.loc[2]<0 or self.loc[3]<0 or self.missing_frame>100:
+        
+        if self.loc[0]<0 or self.loc[1]<0 or self.loc[2]<0 or self.loc[3]<0 or self.missing_frame>100 or center[1]>=h*0.90:
             self.status=False
             self.tracker=None
 
-    def updateTracker(self,frame):
+    def updateTracker(self,frame,w,h):
         if self.status==False:
             return
         ok,rect=self.tracker.update(frame)
         self.status=ok
-        self.updateLoc(rect)
+        self.updateLoc(rect,w,h)
         return rect
     
     def startTracking(self,frame):
