f670bdc41085f5471ed8caecfe01dda79a17c265
cvlovers <99385660+cvlovers@users.noreply.github.com>
2022-03-19 15:12:10 +0300

    Updated ticks

--- histograms.py
+++ histograms.py
@@ -4,7 +4,7 @@ import sys
 from matplotlib import pyplot as plt
 
 
-file = open(sys.argv[1])  # File content is gonna be result.json
+file = open(sys.argv[1])
 data = json.load(file)
 
 
@@ -17,6 +17,7 @@ for item in data:
     speed = abs((data[item]["pos"][-1]["x"] - data[item]["pos"][0]["x"]**2) -
                 (data[item]["pos"][-1]["y"] - data[item]["pos"][0]["y"]**2)) ** 0.5 / frame_count
     speed = "%.2f" % speed
+    speed = float(speed)
 
     d = {"Sheep ID": data[item]["id"], "Frame Count": frame_count, "Sheep Speed": speed}
 
@@ -33,14 +34,16 @@ speed_array = np.array(speed_list)
 file.close()
 
 
-# Shows all the speed values
 plt.hist(speed_array, color="lightgreen", bins=25, alpha=0.5)
 plt.ylabel('Sheep Frequency')
 plt.xlabel('Speed Values')
+print(max_speed)
+plt.xticks(np.arange(0, max_speed, 2))
 plt.show()
 
 
-plt.hist(frame_array, color="pink", bins=5, alpha=0.75)
+plt.hist(frame_array, color="pink", bins=100, alpha=0.75)
 plt.ylabel('Sheep Frequency')
 plt.xlabel('Frame Count')
+plt.xticks(np.arange(0, max_frame, 125))
 plt.show()
