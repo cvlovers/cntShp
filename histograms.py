20fa2be09dbf2f8b19980d8547f3f761c48dfc87
cvlovers <99385660+cvlovers@users.noreply.github.com>
2022-03-19 16:41:38 +0300

    Commented out unneeded parts & added descriptions

--- histograms.py
+++ histograms.py
@@ -3,43 +3,47 @@ import numpy as np
 import sys
 from matplotlib import pyplot as plt
 
+""" A script calculating how many frames each sheep spends on the screen & then creates a histogram.
+The histogram shows the frame count intervals and the number of sheep having frame counts in those intervals.
+"""
 
 file = open(sys.argv[1])
 data = json.load(file)
 
+# Speed data is currently thought to be unneeded, and thus is commented out
 
 frame_list = []
-speed_list = []
+# speed_list = []
 
 
 for item in data:
     frame_count = data[item]["pos"][-1]["frame"] - data[item]["pos"][0]["frame"]
-    speed = abs((data[item]["pos"][-1]["x"] - data[item]["pos"][0]["x"]**2) -
-                (data[item]["pos"][-1]["y"] - data[item]["pos"][0]["y"]**2)) ** 0.5 / frame_count
-    speed = "%.2f" % speed
-    speed = float(speed)
+    # speed = abs((data[item]["pos"][-1]["x"] - data[item]["pos"][0]["x"]**2) -
+                # (data[item]["pos"][-1]["y"] - data[item]["pos"][0]["y"]**2)) ** 0.5 / frame_count
+    # speed = "%.2f" % speed
+    # speed = float(speed)
 
-    d = {"Sheep ID": data[item]["id"], "Frame Count": frame_count, "Sheep Speed": speed}
+    # d = {"Sheep ID": data[item]["id"], "Frame Count": frame_count, "Sheep Speed": speed}
 
     frame_list.append(frame_count)
     speed_list.append(speed)
 
 max_frame = max(frame_list)
-max_speed = max(speed_list)
+# max_speed = max(speed_list)
 
 frame_array = np.array(frame_list)
-speed_array = np.array(speed_list)
+# speed_array = np.array(speed_list)
 
 
 file.close()
 
 
-plt.hist(speed_array, color="lightgreen", bins=25, alpha=0.5)
-plt.ylabel('Sheep Frequency')
-plt.xlabel('Speed Values')
-print(max_speed)
-plt.xticks(np.arange(0, max_speed, 2))
-plt.show()
+# plt.hist(speed_array, color="lightgreen", bins=25, alpha=0.5)
+# plt.ylabel('Sheep Frequency')
+# plt.xlabel('Speed Values')
+# print(max_speed)
+# plt.xticks(np.arange(0, max_speed, 2))
+# plt.show()
 
 
 plt.hist(frame_array, color="pink", bins=100, alpha=0.75)
